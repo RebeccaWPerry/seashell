cfe1ee4676a07560ec22e124db8b89d6b7a3e440
RebeccaWPerry <perry.becca@gmail.com>
2015-10-19 18:39:29 -0400

    tests done for corp function, working on bump finding function and then tests

--- bumps.py
+++ bumps.py
@@ -8,9 +8,12 @@
  # Rebecca W. Perry
 
  # TODO: make radius independent from padding
- # TODO: how do the results depend on choice of radius, searchpoints, and padding?
+ # TODO: how do the results depend on choice of radius, searchpoints, 
+        # and padding?
  # TODO: linking in 3D, more clustering algorithms?
  # TODO: read about convex hulls
+ # TODO: include the power of the point-- prominence, how many contibuted to
+ #       vote for that point
  
 from skimage import data, io
 from skimage.filter import threshold_otsu, sobel
@@ -41,21 +44,34 @@ def crop(binary_image, padding=0):
     border_color = binary_image[0,0] #determines if the border is black or white
     rowsum = np.where(binary_image.sum(1)!=border_color*cols)[0]
     colsum = np.where(binary_image.sum(0)!=border_color*rows)[0]
-    # TODO: return error if padding will cause indices outside image size
+
     [top, bottom] = [rowsum.min()-padding, rowsum.max()+padding+1]
     [left, right] = [colsum.min()-padding, colsum.max()+padding+1]
+    
+    assert top>=0, "padding value, %r pixels, is too large" % padding
+    assert bottom<rows, "padding value, %r pixels, is too large" % padding
+    assert left>=0, "padding value, %r pixels, is too large" % padding
+    assert right<cols, "padding value, %r pixels, is too large" % padding
+
     smaller = binary_image[top:bottom, left:right]
     return smaller, [top, bottom, left, right]
 
+
 def locate_bumps(image, radius=2, searchpoints=1000, display=False):
-    '''Locates bumps around the exterior of a shape in an image.
+    '''Locates bumps around the exterior of a shape in a binarized image.
+       Works in the frame of reference where the object is white and background 
+       is black. Will convert images that don't follow this convention.
+
     Args:
-        image (str or binary image): image file or image in which to find bumps
-        # TODO: allow for black on white or white on black
+        image (image or str): binary image in which to find bumps, or path to a 
+        file containing the image (can be grayscale)
+
     Keyword arguments:
-        radius (float): 
+        radius (float): size of the oval around the object to calculate minimum 
+        distances from. In units of 1/2 major and minor axes.
         
-        searchpoints (int):
+        searchpoints (int): number of points to calculate distances from 
+        (default 1000)
                 
         display (bool): set to True to see plots of the results (defalut False)
 
@@ -68,80 +84,82 @@ def locate_bumps(image, radius=2, searchpoints=1000, display=False):
     else:
         binary = image
 
-    if sum(binary)/(1.0*np.size(binary))>.60:
-        #sign that there is no object in the image
-        master_coords= []
-        print 'no object?'
-    else:
-        binary, crop_location = crop(binary, padding = 2)
+    if binary[0,0] == True:
+        binary = -binary
 
-        center = np.array(np.shape(binary))/2.
-        edges = sobel(binary)
-        edgepixs = np.array(np.where(edges>0)).T
+    binary, crop_location = crop(binary, padding = 2) #small pad for edges
 
-        #find bumps using minimum distance to a bounding ellipse
-        thetas = np.linspace(0,2*pi,searchpoints)
-        ringpoints = np.array([center[1]+radius*center[1]*np.cos(thetas), 
-                               center[0]+radius*center[0]*np.sin(thetas)]).T
-        d = spatial.distance.cdist(ringpoints,edgepixs)
-        votes = edgepixs[np.argmin(d,1)] #each ringpoint votes once
+    center = np.array(np.shape(binary))/2.
+    edges = sobel(binary) #highlights pixels on either side of the edge
+    edges = logical_and(edges,binary)
+    edgepixs = np.array(np.where(edges>0)).T
 
-
-        # group candidates that are very close together, and probably represent the same bump
-        selected_candidates = votes.T
-        points = votes
-        z = hac.linkage(points, method='single')
-        knee = -1 * np.diff(z[::-1, 2], 1)
-        num_clust1 = knee[10:].argmax() + 10 + 1 # guess of number of clusters
-
-        n = 3
-        while n>2: #don't let any center of mass be more than 2 pixels from the original edge
-            part1 = hac.fcluster(z, num_clust1, 'maxclust')
-            final = zeros([len(set(part1)),2])
-
-            for cluster in set(part1):
-                final[cluster-1]  = mean(points[part1 == cluster],0)
-
-            ed = array(where(edges)).T
-            t = spatial.distance.cdist(array([ed[:,0],ed[:,1]]).T,final)
-            dist_from_border = amin(t,0)
-            print dist_from_border
-            n = max(dist_from_border)
-            num_clust1 += 1
-        # end of reducing candidates
+    #find bumps using minimum distance to a bounding ellipse
+    thetas = np.linspace(0,2*pi,searchpoints)
+    ringpoints = np.array([center[1]+radius*center[1]*np.cos(thetas), 
+                           center[0]+radius*center[0]*np.sin(thetas)]).T
+    d = spatial.distance.cdist(ringpoints,edgepixs)
+    votes = edgepixs[np.argmin(d,1)] #each ringpoint votes once
 
 
-        # reset coordinates to the whole frame
-        master_coords = np.array([final[:,1]+crop_location[2],final[:,0]+crop_location[0]]).T
+    # group candidates that are very close together, and probably represent the same bump
+    selected_candidates = votes.T
+    points = votes
+    z = hac.linkage(points, method='single')
+    knee = -1 * np.diff(z[::-1, 2], 1)
+    num_clust1 = knee[10:].argmax() + 10 + 1 # guess of number of clusters
 
-        if display==True:
+    n = 3
+    while n>2: #don't let any center of mass be more than 2 pixels from the original edge
+        part1 = hac.fcluster(z, num_clust1, 'maxclust')
+        final = zeros([len(set(part1)),3])
 
-            plt.figure(figsize=[20,6])
+        for cluster in set(part1):
+            final[cluster-1,0:2]  = mean(points[part1 == cluster],0)
+            final[cluster-1,2] = sum(part1==cluster)
 
-            plt.subplot(1,3,1)
-            io.imshow(image)
-            plt.title('Raw Data')
+        ed = array(where(edges)).T
+        t = spatial.distance.cdist(array([ed[:,0],ed[:,1]]).T,final[:,0:2])
+        dist_from_border = amin(t,0)
+        print dist_from_border
+        n = max(dist_from_border)
+        num_clust1 += 1
+    # end of reducing candidates
 
-            plt.subplot(1,3,2)
-            io.imshow(binary)
-            plt.gray()
-            plt.plot(selected_candidates[1],selected_candidates[0],'ro')
-            plt.xlim(0,np.shape(binary)[1])
-            plt.ylim(np.shape(binary)[0],0)
-            plt.title('First Guess at Peak Locations:\n'+str(np.shape(selected_candidates[1])[0])+ 
-                      ' candidates' )
 
-            plt.subplot(1,3,3)
-            io.imshow(image)
-            plt.plot(master_coords[:,0],master_coords[:,1],'ro')
-            plt.title('Refined Peak Locations Shown on Raw Data:\n'+str(np.shape(final)[0])+ 
-                      ' peaks')
-            plt.xlim(0,np.shape(image)[1])
-            plt.ylim(np.shape(image)[0],0)
+    # translate coordinates to their values in the whole frame
+    master_coords = np.array([final[:,1]+crop_location[2],
+                              final[:,0]+crop_location[0],final[:,2]]).T
 
-            io.show()
+    if display==True:
+
+        plt.figure(figsize=[20,6])
+
+        plt.subplot(1,3,1)
+        io.imshow(image)
+        plt.title('Raw Data')
+
+        plt.subplot(1,3,2)
+        io.imshow(binary)
+        plt.gray()
+        plt.plot(selected_candidates[1],selected_candidates[0],'ro')
+        plt.xlim(0,np.shape(binary)[1])
+        plt.ylim(np.shape(binary)[0],0)
+        plt.title('First Guess at Peak Locations:\n'+str(np.shape(selected_candidates[1])[0])+ 
+                  ' candidates' )
+
+        plt.subplot(1,3,3)
+        io.imshow(image)
+        plt.plot(master_coords[:,0],master_coords[:,1],'ro')
+        plt.title('Refined Peak Locations Shown on Raw Data:\n'+str(np.shape(final)[0])+ 
+                  ' peaks')
+        plt.xlim(0,np.shape(image)[1])
+        plt.ylim(np.shape(image)[0],0)
+
+        io.show()
 
     return master_coords
+
 
 def batch_locate(directory='here'):
     '''Batch processin version of locate_bumps.
---
+++ test_bumps.py
@@ -0,0 +1,45 @@
+# from terminal in this directory, run py.test
+
+from bumps import crop
+from numpy import ones, sum, mgrid
+from numpy.random import rand
+
+# first simple test image is a square of 0's on a background of 1's
+square = ones([200,200])
+square = square>0
+square[50:120,50:130] = False
+
+# second simple test image is a circle of 1's on a bg of 0's
+rows, cols = mgrid[-100:100,-100:100]
+circle = (rows**2+cols**2) < 650
+
+def test_crop_size_on_square():
+    res  = crop(square)
+    assert sum(res[0])==0
+
+def test_crop_size_on_circle():
+    res  = crop(circle)
+    assert sum(res[0])==2029
+
+def test_crop_size_on_square_with_padding():
+    res  = crop(square,4)
+    assert sum(res[0])==1264
+
+def test_crop_location_on_square():
+    res  = crop(square)
+    assert res[1]==[50,120,50,130]
+
+def test_crop_location_on_rectangle():
+    random_rect = ones([300,200])
+    random_rect = random_rect>0
+    a = int(rand()*300)
+    b = int(rand()*(300-(a+1)))+a+1
+
+    c = int(rand()*200)
+    d = int(rand()*(200-(c+1)))+c+1
+
+    print [a,b,c,d]
+
+    random_rect[a:b,c:d] = False
+    res  = crop(random_rect)
+    assert res[1]==[a,b,c,d]
